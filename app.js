015af4905a798fcdbc394bd5fe7821b6fd881aab
aryan-sudo <aryanp1.kvpattom@gmail.com>
2022-07-08 22:11:17 +0530

    changed database from local to cloud

--- app.js
+++ app.js
@@ -2,36 +2,37 @@
 const express = require("express");
 const app = express();
 const engine = require("ejs-mate");
+require("dotenv").config();
 const PORT = process.env.PORT || 8080;
-//const ejs = require("ejs");
+const DB_URL = process.env.DB_URL;
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-// const User = require('./models/commentModel');
-const commentRoutes = require('./routes/commentRoutes');
-main().catch(err => console.log(err));
+const commentRoutes = require("./routes/commentRoutes");
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(DB_URL, connectionParams)
+  .then(() => {
+    console.log("Connected to the database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. n${err}`);
+  });
 
-async function main() {
-    await mongoose.connect('mongodb://localhost:27017/data');
-}
-
-
-
-
-
-const path = require('path');
+const path = require("path");
 //const { getUnpackedSettings } = require("http2");
-app.engine('ejs', engine);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
 
-
-app.use('/comments', commentRoutes);
-
-
+app.use("/comments", commentRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Listening at port ${PORT}`);
+  console.log(`Listening at port ${PORT}`);
+  console.log(DB_URL);
 });
