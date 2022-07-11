510fdfe79d73c92f3f8639efec23f830dd8fe60b
aryan-sudo <aryanp1.kvpattom@gmail.com>
2022-07-11 14:23:38 +0530

    added api changes

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,52 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/commentModel');
+const User = require("../models/commentModel");
 
 router.get("/", async (req, res) => {
-    const see = await User.find({});
-    //console.log(see);
-    res.render("comments", { see });
+  const see = await User.find({});
+  //console.log(see);
+  res.render("comments", { see });
 });
 
 router.post("/", async (req, res) => {
-    console.log(req.body);
-    //res.redirect("comments");
-    const gr = req.body;
-    const add = new User(gr);
-    await add.save();
-    res.redirect("comments");
+  console.log(req.body);
+  //res.redirect("comments");
+  const gr = req.body;
+  const add = new User(gr);
+  await add.save();
+  res.redirect("comments");
 });
 
 router.get("/add", async (req, res) => {
-    res.render("add");
+  res.render("add");
+});
+
+router.get("/addapi", async (req, res) => {
+  const finder = await User.find({});
+  res.json(finder);
 });
 
 router.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    //console.log(id);
-    const find = await User.findById(id);
-    //console.log(find);
-    res.render("view", { find });
+  const { id } = req.params;
+  //console.log(id);
+  const find = await User.findById(id);
+  //console.log(find);
+  res.render("view", { find });
 });
 
 router.get("/:id/edit", async (req, res) => {
-    const { id } = req.params;
-    const find = await User.findById(id);
-    res.render("edit", { find });
+  const { id } = req.params;
+  const find = await User.findById(id);
+  res.render("edit", { find });
 });
 
 router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const ig = req.body;
-    const find = await User.findByIdAndUpdate(id, ig, { runValidators: true, new: true });
-    res.redirect(`/comments/${find._id}`);
+  const { id } = req.params;
+  const ig = req.body;
+  const find = await User.findByIdAndUpdate(id, ig, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/comments/${find._id}`);
 });
 
 router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    const del = await User.findByIdAndDelete(id);
-    res.redirect("/comments");
-
+  const { id } = req.params;
+  const del = await User.findByIdAndDelete(id);
+  res.redirect("/comments");
 });
 
 module.exports = router;
